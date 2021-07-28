400ae5e2603530837680394f6ddc3b81f168a2d4
Harry Guan <69989689+huaicodes@users.noreply.github.com>
2021-07-28 09:01:33 -0700

    hyperlink + name change

--- program.py
+++ program.py
@@ -3,8 +3,14 @@ import numpy as np
 from pyo import *
 import random
 import time
+import webbrowser
 
-s = Server(duplex=1, buffersize=1024, winhost='asio',nchnls=2)
+
+def callback(url):
+    webbrowser.open_new(url)
+
+
+s = Server(duplex=1, buffersize=1024, winhost='asio', nchnls=2)
 s.boot()
 s.start()
 pool = ['a', 'f', 's', 'h']
@@ -12,9 +18,7 @@ pool = ['a', 'f', 's', 'h']
 
 def play_sound(slider):
     temp = pool[random.randrange(3)]
-    print(temp)
     if temp == "a":
-        print("madge")
         SND_PATH = './sounds/'
         sndnames = ["sirens", "honking1", "peets"]
         channels = []
@@ -42,7 +46,6 @@ def play_sound(slider):
                 oscillators.append(HRTF(osc[0], azimuth=osc[1], elevation=osc[2]))
         sum(oscillators).out()
     elif temp == "f":
-        print("feardge")
         sound_path = "./sounds/hauntedhouse.wav"
         sound_player = SfPlayer(sound_path, loop=True)
         ele = 30
@@ -51,7 +54,6 @@ def play_sound(slider):
         binaural_renderer = Binaural(sound_player, azimuth=azi, elevation=ele, azispan=0, elespan=0)
         binaural_renderer.out()
     elif temp == "s":
-        print("sadge")
         sound_path = "./sounds/RainforestEnvComp1.wav"
         sound_player = SfPlayer(sound_path, loop=True)
         ele = 30
@@ -60,7 +62,6 @@ def play_sound(slider):
         binaural_renderer = Binaural(sound_player, azimuth=azi, elevation=ele, azispan=0, elespan=0)
         binaural_renderer.out()
     else:
-        print("gone")
         sound_player = SfPlayer("./sounds/carnival_3.wav", loop=False)
         ele = 45
         azi = Sine(0.1).range(0, 360)
@@ -70,13 +71,15 @@ def play_sound(slider):
 
     slider.destroy()
     #placeholder
-    time.sleep(200)
-    done = True
-    # add link with blank window
+    time.sleep(90)
+    quit()
 
 window = tk.Tk()
 window.geometry("600x600")
-
+root = tk.Tk()
+link1 = tk.Label(root, text="Google Form Hyperlink", fg="blue", cursor="hand2")
+link1.pack()
+link1.bind("<Button-1>", lambda e: callback("https://forms.gle/KfTjk8Abzt2yadmR6"))
 
 def createNewWindow(str):
     current_value = tk.DoubleVar()
@@ -110,11 +113,10 @@ def createNewWindow(str):
 def button_pressed(event):
     global pool
     pool.remove(event)
-    print(pool)
     if event == "a":
         createNewWindow("Anger Emotion Scale")
     elif event == "s":
-        createNewWindow("Sadness Emotion Scale")
+        createNewWindow("Sadness/Calmness Emotion Scale")
     elif event == "f":
         createNewWindow("Fear Emotion Scale")
     else:
@@ -134,7 +136,7 @@ button_02 = tk.Button(text="Fear",
                       button_pressed("f"))
 button_02.place(height=300, width=300, relx=0.5)
 
-button_03 = tk.Button(text="Sadness",
+button_03 = tk.Button(text="Sadness/Calmness",
                       highlightbackground="blue",
                       command=lambda:
                       button_pressed("s"))
